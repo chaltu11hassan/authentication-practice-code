6dc6b21b38e760978cd56749f9819a118e05b44f
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-09-06 18:09:59 -0500

    useContext set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
     </Layout>
   );
 }
---
+++ src/store/auth-context.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token; //converts from false to true vice versa
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return <authContextProvider>{props.children}</authContextProvider>;
+};
+
+export default AuthContext;
